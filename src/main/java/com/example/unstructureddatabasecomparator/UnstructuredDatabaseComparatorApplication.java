e60a0a1050c4e030610c66e7774a4203c92f6578
andrej286 <mikistojanovski6@gmial.com>
2023-11-13 22:19:21 +0100

    first commit

---
+++ src/main/java/com/example/unstructureddatabasecomparator/UnstructuredDatabaseComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.unstructureddatabasecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstructuredDatabaseComparatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnstructuredDatabaseComparatorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/unstructureddatabasecomparator/UnstructuredDatabaseComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unstructureddatabasecomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnstructuredDatabaseComparatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
