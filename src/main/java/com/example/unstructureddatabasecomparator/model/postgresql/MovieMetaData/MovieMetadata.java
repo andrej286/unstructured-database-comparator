f8cc16fd1dc42f83b118af58f3f69bb99042182e
Martin Stojanov <martin.stojanov@emitknowledge.com>
2023-12-22 14:51:38 +0100

    movie metadata postgres annotations

--- src/main/java/com/example/unstructureddatabasecomparator/model/postgresql/MovieMetaData/MovieMetadata.java
+++ src/main/java/com/example/unstructureddatabasecomparator/model/postgresql/MovieMetaData/MovieMetadata.java
@@ -1,6 +1,11 @@
 package com.example.unstructureddatabasecomparator.model.postgresql.MovieMetaData;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
@@ -8,13 +13,18 @@ import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
 public class MovieMetadata {
 
   public String adult;
+  @ManyToOne
   public BelongsToCollection Belongs_To_Collection;
   public int budget;
+  @OneToMany
   public List<Genre> genres;
   public String homepage;
+  @Id
   public int id;
   public int imdb_id;
   public String original_language;
@@ -22,11 +32,14 @@ public class MovieMetadata {
   public String overview;
   public double popularity;
   public String poster_path;
+  @OneToMany
   public List<ProductionCompanies> production_companies;
+  @OneToMany
   public List<ProductionCountries> production_countries;
   public Date release_date;
   public double revenue;
   public double runtime;
+  @OneToMany
   public List<SpokenLanguages> spoken_languages;
   public String status;
   public String tagline;
--- src/main/java/com/example/unstructureddatabasecomparator/model/postgresql/MovieMetaData/ProductionCompanies.java
+++ src/main/java/com/example/unstructureddatabasecomparator/model/postgresql/MovieMetaData/ProductionCompanies.java
@@ -1,12 +1,17 @@
 package com.example.unstructureddatabasecomparator.model.postgresql.MovieMetaData;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
 public class ProductionCompanies {
-
+    @Id
     public int Id;
     public String name;
 }
